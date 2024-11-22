2a3b34f4aab82ddf2af7b0a86655effba7abfdbc
Fatima <aksagovafatima982@gmail.com>
2024-11-22 12:44:24 -0800

    Начало работы над проектом

---
+++ src/main/java/com/example/Task_Management_Programs/TaskManagementProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management_Programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementProgramsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Task_Management_Programs/TaskManagementProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management_Programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
